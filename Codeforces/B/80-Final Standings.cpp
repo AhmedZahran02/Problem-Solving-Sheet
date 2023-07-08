e4a88a0e6f1c1a161600437e053faff04fd58500
AhmedZahran02 <ahmed.zahran02@eng-st.cu.edu.eg>
2023-07-08 18:07:33 +0300

    .

---
+++ Codeforces/B/80-Final Standings.cpp
@@ -0,0 +1,54 @@
+// Made by AOZPROGRAMMING - All Rights Reserved :)
+
+#include<bits/stdc++.h>
+
+#define fast1() ios_base::sync_with_stdio(false)
+#define fast2() cin.tie(NULL)
+#define infile() freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define all(v) v.begin(),v.end()
+#define sortv(vec) sort(all(vec)) //sort vector
+#define getdiff(v1,v2,dist) set_difference(all(v1), all(v2),back_inserter(dist)); // search for v1 in v2 if v is empty without order else use search(v1,v2)
+typedef long long ll;
+typedef unsigned long long ull;
+const double pi = 3.14159265358979323846;
+
+using namespace std;
+
+//sort by first pair
+bool sortbyfst(const pair<ll, pair<ll, string> >& a, const pair<ll, pair<ll, string> >& b)
+{
+    if (a.first == b.first) {
+        return a.second.first < b.second.first;
+    }
+    return a.first > b.first;
+}
+
+void solve() {
+    ll n; cin >> n;
+    vector<pair<ll, pair<ll, string> >> ar(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> ar[i].second.second >> ar[i].first;
+        ar[i].second.first = i;
+    }
+    sort(all(ar), sortbyfst);
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ar[i].second.second << " " << ar[i].first << endl;
+    }
+}
+
+int main() {
+    fast1();
+    fast2();
+    //infile();
+    //-------------------------------------------
+    ll t = 1;
+    //cin >> t;
+    while (t)
+    {
+        solve();
+        t--;
+    }
+    return 0;
+}
---
+++ Codeforces/B/81-Farm.cpp
@@ -0,0 +1,60 @@
+// Made by AOZPROGRAMMING - All Rights Reserved :)
+
+#include<bits/stdc++.h>
+
+#define fast1() ios_base::sync_with_stdio(false)
+#define fast2() cin.tie(NULL)
+#define infile() freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define all(v) v.begin(),v.end()
+#define sortv(vec) sort(all(vec)) //sort vector
+#define getdiff(v1,v2,dist) set_difference(all(v1), all(v2),back_inserter(dist)); // search for v1 in v2 if v is empty without order else use search(v1,v2)
+typedef long long ll;
+typedef unsigned long long ull;
+const double pi = 3.14159265358979323846;
+
+using namespace std;
+
+//long long int faster pow func
+ll fastpow(ll b, ll e) {
+    if (!e)
+    {
+        return 1;
+    }
+    if (e & 1)
+    {
+        return b * fastpow(b, e - 1);
+    }
+    ll x = fastpow(b, e / 2);
+    return x * x;
+}
+
+void solve() {
+    ll n; cin >> n;
+    if (n > 2 || n == 0)
+    {
+        cout << -1 << endl;
+    }
+    else if (n == 1)
+    {
+        cout << "1 2 3" << endl;
+    }
+    else if (n == 2)
+    {
+        cout << "3 4 5" << endl;
+    }
+}
+
+int main() {
+    fast1();
+    fast2();
+    //infile();
+    //-------------------------------------------
+    ll t = 1;
+    //cin >> t;
+    while (t)
+    {
+        solve();
+        t--;
+    }
+    return 0;
+}
